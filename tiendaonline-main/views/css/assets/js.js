f1111d5b50435e514ed9906596385df9037842c4
civic100 <114212306+civic100@users.noreply.github.com>
2022-11-17 17:16:46 +0100

    Delete tiendaonline-main directory

--- tiendaonline-main/views/css/assets/js.js
+++
@@ -1,9 +0,0 @@
-//Cambia el type de password a text para mostrar la contraseña.
-function mostrarContrasena(){
-    var tipo = document.getElementById("password");
-    if(tipo.type == "password"){
-        tipo.type = "text";
-    }else{
-        tipo.type = "password";
-    }
-}
